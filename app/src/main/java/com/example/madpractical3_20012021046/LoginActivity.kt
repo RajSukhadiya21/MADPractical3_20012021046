b0ad949eb376a095a3deec55951cb543d6f72cde
RajSukhadiya21 <rajsukhadiya387@gmail.com>
2022-09-01 21:54:13 +0530

    Practical-3

--- app/src/main/java/com/example/madpractical3_20012021046/LoginActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021046/LoginActivity.kt
@@ -1,13 +1,16 @@
 package com.example.madpractical3_20012021046
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import com.example.madpractical3_20012021046.databinding.ActivityLoginBinding
 import com.example.madpractical3_20012021046.databinding.ActivityMainBinding
-import com.example.madpractical3_20012021046.databinding.ActivityRegistrationBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -15,5 +18,24 @@ class LoginActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        val signup = findViewById<TextView>(R.id.signup)
+
+        signup.setOnClickListener {
+
+            Intent(this, RegistrationActivity::class.java).also {startActivity(it)}
+
+            signup.movementMethod = LinkMovementMethod.getInstance();
+
+        }
+
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_login
+        binding.bottomNavigationView.setOnItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.bottom_nav_reg -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
